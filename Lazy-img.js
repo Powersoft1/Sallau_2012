277904e346eacd4514048721ef8b0cb5c54c95e3
Powersoft1 <developmentnaeem@gmail.com>
2025-06-10 22:07:44 +0100

    Update Lazy-img.js
    Start

--- Lazy-img.js
+++ Lazy-img.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll("img").forEach(img => {
         if (!img.hasAttribute("data-src")) {
             img.setAttribute("data-src", img.src); // Store original src
-            img.src = "icon/placeholder.png"; // replace placeholder
+            img.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgKcezqQ6F4OSf0luVskJn4IQpLIIZlDd76_FySr_j09RlRF4PDt8VTAJ2HhvtEypIuEB1tJlaeovoGgu1B92TA996a-coIY_7qHaKFpTTeSrUIZ5R0e35zk7rzL-fr3_usYBhsOMUIety8w-7Hx2GJRVcyFi95bb0yTx6FZnMQW4Xxg4lzW_1sSf3x8WMu/s500/SALLAU%202012.png"; // replace placeholder
 
             img.classList.add("lazy-img");
         }
@@ -20,4 +20,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.querySelectorAll("img.lazy-img").forEach(img => observer.observe(img));
-});
+}); 
